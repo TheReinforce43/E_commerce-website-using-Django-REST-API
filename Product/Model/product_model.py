39d4c66abaeb81f97a91800160eab6ac56a4a06b
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-01-02 20:41:49 +0600

    test the merge

--- Product/Model/product_model.py
+++ Product/Model/product_model.py
@@ -14,8 +14,8 @@ class ProductModel(models.Model):
     quantity = models.IntegerField(default=0)
     color=models.CharField(max_length=50)
 
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    # created_at=models.DateTimeField(auto_now_add=True)
+    # updated_at=models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
