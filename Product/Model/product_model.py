a9ebaf24642936e1d17c31bb3e9b6727b5c7c9a3
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-01-02 20:51:01 +0600

    change in color attribute

--- Product/Model/product_model.py
+++ Product/Model/product_model.py
@@ -12,7 +12,7 @@ class ProductModel(models.Model):
     name = models.CharField(max_length=100,unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(default=0)
-    color=models.CharField(max_length=50)
+    color=models.CharField(max_length=50,null=True, blank=True)
 
     # created_at=models.DateTimeField(auto_now_add=True)
     # updated_at=models.DateTimeField(auto_now=True)
