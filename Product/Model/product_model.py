15c1176a1a8f3b553144d0b19d1e790db7d1b5c1
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-01-02 20:30:24 +0600

    added in color in product model

--- Product/Model/product_model.py
+++ Product/Model/product_model.py
@@ -12,6 +12,7 @@ class ProductModel(models.Model):
     name = models.CharField(max_length=100,unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(default=0)
+    color=models.CharField(max_length=50)
 
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
